9ed74cdc2a2d6e79a1c72e7fe50198b5560fc7c3
Thiruselvam11 <muruganselva986@gmail.com>
2023-02-01 11:53:57 +0530

    New shadow dom component added

--- src/components/ShadowScroll/Shadowscroll.js
+++ src/components/ShadowScroll/Shadowscroll.js
@@ -1,19 +1,24 @@
 import React from "react";
-import './Shadowscroll.css'
+import root from 'react-shadow';
+import Shadowscroll from './Shadowscroll.css'
 
-class Shadowscroll extends React.Component {
-
-    render() {
-        return React.createElement(
-            "div",
-            { className: "content_img" },
-            React.createElement("div", null,
-                React.createElement("h1", { className: "heading" }, "I'm Shadow Component"),
-                React.createElement("p", { className: "description" }, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
-            )
-
+function Shadowscroll(){
+     return (
+            <root.div className="">
+                <q>There is strong shadow where there is much light.</q>
+                <span className="">― Johann Wolfgang von Goethe.</span>
+            </root.div>
         );
     }
-}
 export default Shadowscroll;
+
+// React.createElement(
+//     "div",
+//     { className: "content_img" },
+//     React.createElement("div", null,
+//         React.createElement("h1", { className: "heading" }, "I'm Shadow Component"),
+//         React.createElement("p", { className: "description" }, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
+//     )
+
+// );
 
