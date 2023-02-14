ce6d00356e1447a2eb0133acd57f202b19363a6c
Thiruselvam11 <muruganselva986@gmail.com>
2023-02-14 10:10:23 +0530

    new Cai component added

---
+++ src/components/Caiui/Caiui.js
@@ -0,0 +1,206 @@
+import "./Caiui.css";
+import Cai1 from '../../assets/Cai1.webp'
+import Cai2 from '../../assets/Cai2.webp'
+import Cai3 from '../../assets/Cai3.webp'
+import Cai4 from '../../assets/Cai4.webp'
+import Cai5 from '../../assets/Cai5.webp'
+import Cai6 from '../../assets/Cai6.webp'
+import Cai7 from '../../assets/Cai7.webp'
+import star from '../../assets/star.png'
+
+const Caiui = () => {
+  return (
+    <div className="new-cai">
+      <section className="video-section">
+        <div className="video-section-inner">
+          <video className="wrapper" autoPlay muted loop>
+            <source src="https://asset.weaveroo.io/cai/Heels/maroon-studded-heels/videos/cai.mp4" />
+          </video>
+        </div>
+      </section>
+      <section className="perfectfit">
+        <div className="perfectcontainer">
+          <div className="heading">Perfect fit</div>
+          <p className="paragraph">
+            Bring out your festive personality with poise and maximise comfort
+            by 'opting for a size larger than usual for wider feet.' Mix comfort
+            and style with ease with the Maroon Studded Heels.
+          </p>
+        </div>
+      </section>
+      <section className="desktop-5">
+        <div className="b8af48-94c0-438c-b13b-b24898e1-wrapper">
+          <img
+            className="b8af48-94c0-438c-b13b-b24898e1-icon"
+            alt=""
+            src= {Cai1} />
+        </div>
+        <div className="timeless-and-comfy-parent">
+          <h1 className="timeless-and-comfy">Timeless and comfy</h1>
+          <p className="a-symphony-of">
+            A symphony of beautiful earthy shades with an open toe, Maroon
+            Studded Heels made of PU leather are sustainable, super comfortable
+            and super stylish.
+          </p>
+        </div>
+      </section>
+      <section className="desktop-7">
+        <div className="fashion-meets-comfort-parent">
+          <h1 className="fashion-meets-comfort">Fashion meets comfort</h1>
+          <p className="a-chic-addition">
+            A chic addition to your look, these strappy sandals have super soft
+            nubuck sheet inner soles that provide comfort and are stylish in
+            equal measure. They feel as good as they look!
+          </p>
+        </div>
+        <div className="bf4bbc2-fc97-4098-a059-84c422c-wrapper">
+          <img
+            className="bf4bbc2-fc97-4098-a059-84c422c-icon"
+            alt=""
+            src={Cai2}
+          />
+        </div>
+      </section>
+      <section className="desktop-5">
+        <div className="b8af48-94c0-438c-b13b-b24898e1-wrapper">
+          <img
+            className="ac2693bf-7a07-48d4-a00a-25b67c-icon"
+            alt=""
+            src={Cai3}
+          />
+        </div>
+        <div className="timeless-and-comfy-parent">
+          <h1 className="timeless-and-comfy">Cosy style</h1>
+          <p className="thoughtfully-crafted-footwear">
+            Thoughtfully crafted footwear in PU leather with maroon studded
+            straps and mid heels, which look amazing and feel like the perfect
+            pair of everyday footwear.
+          </p>
+        </div>
+      </section>
+      <section className="desktop-8">
+        <div className="function-fashionably-parent">
+          <p className="function-fashionably">FUNCTION FASHIONABLY!</p>
+          <p className="combining-function-and">
+            Combining function and form, CAI fashionably steers the conscious
+            consumption movement locally and globally with non-leather,
+            pocket-friendly footwear.
+          </p>
+        </div>
+        <div className="be79a2-ab6d-4a12-9499-2f5d55d8-wrapper">
+          <img
+            className="be79a2-ab6d-4a12-9499-2f5d55d8-icon"
+            alt=""
+            src={Cai4}
+          />
+        </div>
+      </section>
+      <section className="gif-section">
+        <img
+          className="gif-section-child"
+          alt=""
+          src="https://syndication-global-v3-wpageassetbucket-ymw4gzd09eqt.s3.amazonaws.com/cai-01/en/maroon-studded-heels/1/image/c1a0732d-378c-4b50-9434-a4d709c126ef.gif"
+        />
+      </section>
+      <section className="magicdetail">
+        <div className="magiccontainer">
+          <img
+            className="desimage-icon"
+            alt=""
+            src={Cai5}
+          />
+          <img
+            className="desimage-icon"
+            alt=""
+            src={Cai6}
+          />
+        </div>
+      </section>
+      <section className="desktop-3">
+        <div className="frame-parent">
+          <div className="customer-reviews-parent">
+            <div className="customer-reviews">Customer Reviews</div>
+            <div className="jg-parent">
+              <div className="jg">J.G</div>
+              <div className="ef-aadb-4f47-9961-fed46d093f01-parent">
+                <img
+                  className="ef-aadb-4f47-9961-fed46d093f01-icon"
+                  alt=""
+                  src={star}
+                />
+                <img
+                  className="ef-aadb-4f47-9961-fed46d093f01-icon"
+                  alt=""
+                  src={star}
+                />
+                <img
+                  className="ef-aadb-4f47-9961-fed46d093f01-icon"
+                  alt=""
+                  src={star}
+                />
+                <img
+                  className="ef-aadb-4f47-9961-fed46d093f01-icon"
+                  alt=""
+                  src={star}
+                />
+                <img
+                  className="ef-aadb-4f47-9961-fed46d093f01-icon"
+                  alt=""
+                  src={star}
+                />
+              </div>
+            </div>
+            <div className="the-cai-store">
+              The cai store has outdone themselves with this collection!!
+            </div>
+          </div>
+          <div className="frame-group">
+            <div className="pg-parent">
+              <div className="pg">P.G</div>
+              <div className="ef-aadb-4f47-9961-fed46d093f01-parent">
+                <img
+                  className="ef-aadb-4f47-9961-fed46d093f01-icon"
+                  alt=""
+                  src={star}
+                />
+                <img
+                  className="ef-aadb-4f47-9961-fed46d093f01-icon"
+                  alt=""
+                  src={star}
+                />
+                <img
+                  className="ef-aadb-4f47-9961-fed46d093f01-icon"
+                  alt=""
+                  src={star}
+                />
+                <img
+                  className="ef-aadb-4f47-9961-fed46d093f01-icon"
+                  alt=""
+                  src={star}
+                />
+                <img
+                  className="ef-aadb-4f47-9961-fed46d093f01-icon"
+                  alt=""
+                  src={star}
+                />
+              </div>
+            </div>
+            <div className="from-the-point">
+              From the point of view of comfort, it is well and good. I love
+              wearing this as its very light and comfy....
+            </div>
+          </div>
+        </div>
+        <div className="bcd071-f99b-436c-bc36-38a5bfa4-wrapper">
+          <img
+            className="bcd071-f99b-436c-bc36-38a5bfa4-icon"
+            alt=""
+            src={Cai7}
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Caiui;
---
+++ src/stories/Caiui.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Caiui from "../components/Caiui/Caiui";
+
+export default {
+     title: "Cai/Caiui",
+     component: Caiui
+}
+
+export const caicomponent = (args) => <Caiui {...args} />
